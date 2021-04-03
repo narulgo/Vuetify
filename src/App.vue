6ffa93a683a063969dcd2ebaab8d14f8cfbdc4e5
Narul <naruln9494@gmail.com>
2021-04-04 02:16:30 +0600

    Video Player

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main>
+      <div
+        class="d-flex align-center mx-auto fill-height"
+        :style="{ width: '60%' }"
+      >
+        <Player :source="{ src, type: 'video/mp4' }" />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Player from "./components/Player"
+
+export default {
+  name: "App",
+
+  components: {
+    Player
+  },
+
+  data: () => ({
+    src: require("@/assets/video.mp4")
+  })
+}
+</script>
---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,144 @@
+<template>
+  <div :class="classes" @mousedown="onSliderMouseDown" @click="onSliderClick">
+    <div class="slider__bar" ref="bar">
+      <div class="slider__handle" :style="handleStyle" ref="handle" />
+      <div class="slider__fill" :style="fillStyle" />
+      <slot name="bar" />
+    </div>
+  </div>
+</template>
+
+<script>
+import './Slider.scss'
+import throttle from 'lodash/throttle'
+import { getRelativeXPosition } from '@/utils/helpers'
+
+const lerp = require('lerp');
+
+export default {
+  name: 'Slider',
+
+  props: {
+    min: {
+      type: Number,
+      default: 0
+    },
+
+    max: {
+      type: Number,
+      default: 1
+    },
+
+    value: Number,
+
+    disabled: Boolean
+  },
+
+  data: () => ({
+    isDragging: false,
+    handleWidth: 0,
+    barWidth: 0,
+    dragTimeout: null
+  }),
+
+  computed: {
+    classes () {
+      return {
+        slider: true,
+        'slider--dragging': this.isDragging,
+        'slider--disabled': this.disabled
+      }
+    },
+
+    delta () {
+      return this.value / this.max
+    },
+
+    fillStyle () {
+      return {
+        transformOrigin: 'left center',
+        transform: `scaleX(${this.delta})`
+      }
+    },
+
+    handleStyle () {
+      const { barWidth, delta, handleWidth } = this
+
+      return {
+        transform: `translateX(${barWidth * delta - handleWidth * 0.5}px)`
+      }
+    }
+  },
+
+  mounted () {
+    this.onWindowResize = throttle(this.onWindowResize, 200)
+
+    this.calcDimensions()
+
+    window.addEventListener('resize', this.onWindowResize)
+    document.addEventListener('mouseup', this.onDocumentMouseUp)
+    document.addEventListener('mousemove', this.onDocumentMouseMouseMove)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onWindowResize)
+    document.removeEventListener('mouseup', this.onDocumentMouseUp)
+    document.removeEventListener('mousemove', this.onDocumentMouseMouseMove)
+  },
+
+  methods: {
+    onWindowResize () {
+      this.calcDimensions ()
+    },
+
+    onDocumentMouseMouseMove (e) {
+      if (this.disabled || !this.isDragging) return
+      this.calculate(e)
+    },
+
+    onDocumentMouseUp () {
+      if (this.disabled) return
+
+      if (this.dragTimeout) {
+        clearTimeout(this.dragTimeout)
+        this.$emit('click')
+      } else {
+        this.isDragging = false
+        this.$emit('dragend')
+      }
+    },
+
+    onSliderClick (e) {
+      if (this.disabled) return
+
+      this.calculate(e)
+    },
+
+    onSliderMouseDown () {
+      if (this.disabled) return
+
+      this.dragTimeout = setTimeout(() => {
+        this.isDragging = true
+        this.dragTimeout = null
+
+        this.$emit('dragstart')
+      }, 100);
+    },
+
+    calcDimensions () {
+      const { bar, handle } = this.$refs
+
+      this.handleWidth = handle.offsetWidth
+      this.barWidth = bar.offsetWidth
+    },
+
+    calculate (e) {
+      const { bar } = this.$refs
+      const { barWidth } = this
+      const relativeX = getRelativeXPosition(e, bar)
+      const delta = relativeX / barWidth
+      this.$emit('input', lerp(this.min, this.max, delta))
+    }
+  }
+}
+</script>
